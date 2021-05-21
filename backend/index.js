0450333415b6501fe015e450fbe66a12ae0a6b64
garrettruss <garrettmruss@gmail.com>
2021-05-21 09:47:41 -0400

    update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,28 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500,
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
+)
+.catch(err => {
+    console.log.error(err.stack)
+    process.exit(1)
+})
+.then(async client =>  {
+    await RestaurantsDAO.injectDB(client)
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
+})
